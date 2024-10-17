0c7b62b92b1082e29b683acb5ef7a204176022a0
Jeffrey V <luanvecino@hotmail.com>
2024-10-16 23:16:56 -0500

    First version

---
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -0,0 +1,39 @@
+export const Inicio = () => {
+    return (
+        <>
+            Hola usuario! <br />
+            Bienvenidos a esta pagina!
+            <br />
+            
+
+        
+
+
+            La profe luz es la mejor maestra de la CUC, la quiero profe <br />
+           
+            
+
+            Estas son las razones! <br />
+            
+
+            <ol>
+                <li>
+                    Explica muy bien!
+                </li>
+                <br/>
+
+                <li>
+                    Da buenos consejos
+                </li>
+                <br />
+
+                <li>
+                    Me hizo darme cuenta que mi verdadera pasion es el Front-end
+                </li>
+
+            </ol>
+        </>
+    );
+
+
+};
